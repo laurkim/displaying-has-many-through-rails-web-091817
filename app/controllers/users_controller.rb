8958fbefd4bb1344197c42930546cb4e12842ea8
laurkim <laurakeem@gmail.com>
2017-10-19 08:05:53 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
   def show
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email)
   end
 end
